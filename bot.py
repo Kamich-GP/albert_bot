4ead12747d1b0c7bd2ad18e9556124a57ba5050c
Kamich-GP <yurochkagameplus@gmail.com>
2024-05-11 22:00:58 +0500

    work with db

--- bot.py
+++ bot.py
@@ -5,14 +5,14 @@ from geopy import Nominatim
 
 
 # Создаем объект бота
-bot = telebot.TeleBot('7117428954:AAGYDL9dS7gtQghi1eujdNom65-mRKg1vQ4')
+bot = telebot.TeleBot('TOKEN')
 # Работа с картами
 geolocator = Nominatim(user_agent='Mozilla/5.0 '
                                   '(Windows NT 10.0; Win64; x64) '
                                   'AppleWebKit/537.36 (KHTML, like Gecko) '
                                   'Chrome/124.0.0.0 Safari/537.36')
 
-
+## Сторона пользователя ##
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(msg):
@@ -73,6 +73,91 @@ def get_loc(msg, user_name, user_num):
         bot.send_message(user_id, 'Отправьте локацию через кнопку!')
         # Возврат на этап получения локации
         bot.register_next_step_handler(msg, get_loc, user_name, user_num)
+
+
+## Сторона администратора ##
+# Обработчик команды /admin
+@bot.message_handler(commands=['admin'])
+def start_admin(msg):
+    user_id = msg.from_user.id
+    if user_id == 6775701667:
+        bot.send_message(user_id, 'Добро пожаловать в админ панель!',
+                         reply_markup=bt.admin_menu())
+        # Переход на этап выбора админа
+        bot.register_next_step_handler(msg, admin_choice)
+    else:
+        bot.send_message(user_id, 'Вы не админ!')
+
+
+# Этап выбора админа
+def admin_choice(msg):
+    admin_id = msg.from_user.id
+    if msg.text == 'Добавить продукт':
+        bot.send_message(admin_id, 'Напишите наименование товара!',
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+        # Переход на этап получения названия
+        bot.register_next_step_handler(msg, get_pr_name)
+    elif msg.text == 'Удалить продукт':
+        bot.send_message(admin_id, 'Выберите товар!')
+    elif msg.text == 'Изменить количество':
+        bot.send_message(admin_id, 'Выберите товар!')
+
+
+# Этап получения названия
+def get_pr_name(msg):
+    admin_id = msg.from_user.id
+    pr_name = msg.text
+    bot.send_message(admin_id, 'Теперь придумайте описание товару!')
+    # Переход на этап получения описания
+    bot.register_next_step_handler(msg, get_pr_des, pr_name)
+
+
+# Этап получения описания
+def get_pr_des(msg, pr_name):
+    admin_id = msg.from_user.id
+    pr_des = msg.text
+    bot.send_message(admin_id, 'Теперь введите цену товара!')
+    # Переход на этап получения цены
+    bot.register_next_step_handler(msg, get_pr_price, pr_name, pr_des)
+
+
+# Этап получения цены
+def get_pr_price(msg, pr_name, pr_des):
+    admin_id = msg.from_user.id
+    if msg.text.isdecimal():
+        pr_price = float(msg.text)
+        bot.send_message(admin_id, 'Перейдите на сайт https://postimages.org/\n'
+                                   'Загрузите фото товара и отправьте мне прямую ссылку на него!')
+        # Переход на этап получения фото
+        bot.register_next_step_handler(msg, get_pr_photo, pr_name, pr_des, pr_price)
+    else:
+        bot.send_message(admin_id, 'Отправьте цену цифрами!')
+        # Возврат на этап получения цены
+        bot.register_next_step_handler(msg, get_pr_price, pr_name, pr_des)
+
+
+# Этап получения названия
+def get_pr_photo(msg, pr_name, pr_des, pr_price):
+    admin_id = msg.from_user.id
+    pr_photo = msg.text
+    bot.send_message(admin_id, 'Какое количество у товара?')
+    # Переход на этап получения количества
+    bot.register_next_step_handler(msg, get_pr_count, pr_name, pr_des, pr_price, pr_photo)
+
+
+# Этап получения количества
+def get_pr_count(msg, pr_name, pr_des, pr_price, pr_photo):
+    admin_id = msg.from_user.id
+    if msg.text.isnumeric():
+        pr_count = int(msg.text)
+        db.add_pr(pr_name, pr_des, pr_price, pr_photo, pr_count)
+        bot.send_message(admin_id, 'Товар успешно добавлен!', reply_markup=bt.admin_menu())
+        # Переход на админ панель
+        bot.register_next_step_handler(msg, start_admin)
+    else:
+        bot.send_message(admin_id, 'Отправьте цену цифрами!')
+        # Возврат на этап получения количества
+        bot.register_next_step_handler(msg, get_pr_count, pr_name, pr_des, pr_price, pr_photo)
 
 
 # Запуск бота
--- buttons.py
+++ buttons.py
@@ -23,3 +23,28 @@ def loc_button():
     # Добавляем кнопки в пространство
     kb.add(but1)
     return kb
+
+
+# Кнопки админ-меню
+def admin_menu():
+    # Создаем пространство
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # Создаем сами кнопки
+    add = types.KeyboardButton('Добавить продукт')
+    delete = types.KeyboardButton('Удалить продукт')
+    change = types.KeyboardButton('Изменить количество')
+    back = types.KeyboardButton('Обратно в меню')
+    # Добавляем кнопки в пространство
+    kb.add(add, delete, change)
+    kb.row(back)
+
+
+# Кнопки для подтверждения
+def confirm_buttons():
+    # Создаем пространство
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # Создаем сами кнопки
+    yes = types.KeyboardButton('Да')
+    no = types.KeyboardButton('Нет')
+    # Добавляем кнопки в пространство
+    kb.add(yes, no)
